08f4c17b63f702bffd5be469ecdcb33a3d77279e
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-07-30 16:31:20 +0300

    create the validation form and a readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
 
 $(document).ready(function(){
 // backend
-
+ 
 // form validation
-$("form#form").submit(function(event){
-    var inputtedFirstName = $("input#firstName").text();
-    var inputtedLastName =$("input#lastName")
+$("form#form1").submit(function(event){
+    var inputtedFirstName = $("input#firstName").val();
+    var inputtedLastName =$("input#lastName").val();
     var inputtedDate=$("input#date").val();
     var inputtedAge = $("input#age").val();
     function validate(){
-        if($(inputtedFirstName) ==="") {
-            alert("Please enter your name")
-        }else if($(inputtedLastName)===""){
-            alert("please enter your last name")
-        }else if($(inputtedAge ) <13){
-            alert("your aage is below minimum")
-        }else if($(inputtedAge)>50){
-            alert("your age is above maximum")
-        }else if($(inputtedDate)===""){
-            alert("please enter a valid date")
+        if((inputtedFirstName || inputtedLastName) ===""){
+            alert("please enter correct data on first and last name");
+        }else if(inputtedAge <13){
+            alert("Your age is below minimum [13]")
+        }else if (inputtedAge >55) {
+            alert("your age is above maximim [55]")
         }else{
-            alert("your have inputted correct data")
+         return false;
         }
+       
     }
-    console.log(date)
-    console.log(name);
-    console.log(age);
-
-    event.preventdefault(); 
+    validate();
+    
+    event.preventDefault(); 
 });
 
 });
